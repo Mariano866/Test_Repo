66cb281900b3c9e35217feacb2a13e5ec5b30bd5
Mariano <mgiraldi@infraestructura.gob.ar>
2026-05-14 15:31:56 -0300

    Se suben los archivos .cs correctos

---
+++ API_Testing/API_Testing/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Testing;
+[ApiController]
+[Route("api/[controller]")]
+public class PingController : ControllerBase
+{   
+   [HttpGet("ping")]
+   public IActionResult Ping()
+   {
+    return Ok("pong");
+   }
+    
+}
